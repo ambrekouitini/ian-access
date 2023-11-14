b69a2cfce4c5153bf4773f4afe1bb029d502431e
MEHDI BELLAM <porcepic91@gmail.com>
2023-11-14 16:40:49 +0100

    ajout du javascript pour l'ajout du boutton play

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,22 @@
 //Script JS INDEX
-
-const speech = new SpeechSynthesisUtterance();
-document.getElementById("btn").addEventListener("click", () => { 
-
-  var textToSpeech = "Text"
-  const utterance = new SpeechSynthesisUtterance(textToSpeech);
-  speechSynthesis.speak(utterance);
-
+const allTxt = document.querySelectorAll("h1 , p ");
+// add a button to all allTxt
+allTxt.forEach((txt) => {
+  const btn = document.createElement("button");
+  btn.className += "btn";
+  
+  btn.backgroundImage = "url(assets/1.png)";
+  txt.appendChild(btn);
 });
+
+allTxt.forEach((txt) => {
+  const speech = new SpeechSynthesisUtterance();
+  document.getElementById("btn").addEventListener("click", () => {
+
+    var textToSpeech = txt.textContent;
+    const utterance = new SpeechSynthesisUtterance(textToSpeech);
+    speechSynthesis.speak(utterance);
+
+  });
+});
+
